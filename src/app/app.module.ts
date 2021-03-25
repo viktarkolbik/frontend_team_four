7c1dabdaea1f5f16b1e8a77e6a5b2cc61dc95cd8
Yaroslav Zhironkin <rainsolnce@gmail.com>
2021-03-25 12:15:29 +0300

    feat: create regform component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegformComponent } from './regform/regform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegformComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/regform/regform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ia-regform',
+  templateUrl: './regform.component.html',
+  styleUrls: ['./regform.component.scss']
+})
+export class RegformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
