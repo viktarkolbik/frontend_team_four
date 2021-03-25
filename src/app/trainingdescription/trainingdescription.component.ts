e0e97b56ff25cd722a896314f0e5c2a9834bf315
Alekskarb <korbutowski@gmail.com>
2021-03-25 12:29:50 +0300

    feat: add trainingdescription

---
+++ src/app/trainingdescription/trainingdescription.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ia-trainingdescription',
+  templateUrl: './trainingdescription.component.html',
+  styleUrls: ['./trainingdescription.component.scss']
+})
+export class TrainingdescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
