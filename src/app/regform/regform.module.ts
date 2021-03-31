6e71d22785870391252a7a912adbce64284088d9
RuslanTanasiichuk <74545458+RuslanTanasiichuk@users.noreply.github.com>
2021-03-31 15:46:37 +0300

    filling the form

--- src/app/regform/regform.module.ts
+++ src/app/regform/regform.module.ts
@@ -2,6 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RegformRoutingModule } from './regform-routing.module';
 import { RegformComponent } from './regform.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatChipsModule} from '@angular/material/chips';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatRadioModule} from "@angular/material/radio";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -9,7 +17,16 @@ import { RegformComponent } from './regform.component';
   ],
   imports: [
     BrowserModule,
-    RegformRoutingModule
+    RegformRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatRadioModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
   ],
   providers: [],
   exports: [RegformComponent]
