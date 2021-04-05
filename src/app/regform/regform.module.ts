2a92bfd19263415e9d9a5efda4d930687de72062
Darya <nikiporonokdarya@gmail.com>
2021-04-05 22:51:56 +0300

    feat: add header and footer

--- src/app/regform/regform.module.ts
+++ src/app/regform/regform.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RegformRoutingModule } from './regform-routing.module';
 import { RegformComponent } from './regform.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import {MatChipsModule} from '@angular/material/chips';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {MatRadioModule} from "@angular/material/radio";
-import {MatCheckboxModule} from "@angular/material/checkbox";
-import {MatButtonModule} from "@angular/material/button";
+import { MatFormFieldModule}  from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatChipsModule } from '@angular/material/chips';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatButtonModule } from "@angular/material/button";
 import { HeaderModule } from '../header/header.module';
+import { FooterModule } from '../footer/footer.module';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { HeaderModule } from '../header/header.module';
     FormsModule,
     ReactiveFormsModule,
     MatCheckboxModule,
-    HeaderModule
+    HeaderModule,
+    FooterModule
   ],
   providers: [],
   exports: [RegformComponent]
--- src/app/trainingdescription/trainingdescription.module.ts
+++ src/app/trainingdescription/trainingdescription.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HeaderModule } from '../header/header.module';
 
 import { TrainingdescriptionRoutingModule } from './trainingdescription-routing.module';
 import { TrainingdescriptionComponent } from './trainingdescription.component';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { FooterModule } from '../footer/footer.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +15,7 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserModule,
     TrainingdescriptionRoutingModule,
     HeaderModule,
+    FooterModule,
     MatButtonModule
   ],
   providers: [],
