971079aa53416374445091427ebb3f134ec4d040
Роман Неред <romaus@mail.ru>
2021-04-09 00:57:24 +0300

    feat: localstorage service with set and get lang method

--- src/app/core/localstorage.service.ts
+++ src/app/core/localstorage.service.ts
@@ -4,6 +4,12 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class LocalstorageService {
-
+  static localStorage: Storage = window.localStorage;
+  static getLang(): string | null {
+    return this.localStorage.getItem('lang');
+  }
+  static setLang(value: any): void {
+    this.localStorage.setItem('lang', value);
+  }
   constructor() { }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
+import {LocalstorageService} from '../core/localstorage.service';
 
 @Component({
   selector: 'ia-header',
@@ -7,13 +8,26 @@ import {TranslateService} from '@ngx-translate/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  public selected = 'ru';
   constructor(public translate: TranslateService) {
     translate.addLangs(['en', 'ru']);
-    translate.setDefaultLang('ru');
   }
+  public selectedLang: any;
+  public changeLang(): void {
+    LocalstorageService.setLang(this.selectedLang);
+    this.translate.use(this.selectedLang);
+}
 
   ngOnInit(): void {
+    if (!LocalstorageService.getLang()) {
+      const langs = this.translate.getLangs();
+      const browserLang = this.translate.getBrowserLang();
+      this.selectedLang = ((langs.indexOf(browserLang)) === -1) ? this.translate.getDefaultLang() : browserLang;
+      LocalstorageService.setLang(this.selectedLang);
+      this.translate.use(this.selectedLang);
+    }
+    else {
+      this.selectedLang = LocalstorageService.getLang();
+      this.translate.use(this.selectedLang);
+    }
   }
-
 }
