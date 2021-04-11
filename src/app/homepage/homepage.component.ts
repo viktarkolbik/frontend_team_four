918cc2e9720fe7abb1aac64afbf6eb7c4b099fc1
Yaroslav Zhironkin <rainsolnce@gmail.com>
2021-04-11 23:35:13 +0300

    feat: implement filters

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,45 +1,170 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Training } from '../types';
 import { InternshipsService } from '../core/internships.service';
-import { MatOptionSelectionChange } from '@angular/material/core';
-
 
 @Component({
   selector: 'ia-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss']
+  styleUrls: ['./homepage.component.scss'],
 })
-
 export class HomepageComponent implements OnInit {
   trainings: Training[];
   filtered!: Training[];
+
+  activeFilters = {
+    countryFilter: false,
+    cityFilter: false,
+    techFilter: false
+  }
+
+  filtersState = [
+    {
+      html: false,
+      javascript: false,
+      css: false,
+    },
+    {
+      belarus: false,
+      russia: false,
+      ukraine: false,
+    },
+    {
+      minsk: false,
+      gomel: false,
+      brest: false,
+      kiev: false,
+      chernigov: false,
+      harkov: false,
+      moscow: false,
+    },
+  ]
 
   constructor(private internshipsService: InternshipsService) {
     this.trainings = this.internshipsService.getTrainingsLocal();
   }
 
   ngOnInit(): void {
-    this.internshipsService.getInternshipList().subscribe((data) => console.log(data));
+    this.internshipsService
+      .getInternshipList()
+      .subscribe((data) => console.log(data));
   }
 
-  filterLocation(event: any){
-    const locale = event.source.value;
-    console.log(event);
-    if(event.source._selected){
-      this.filtered = this.trainings.filter(item => item.city.toLowerCase().includes(locale.split(' ')[1]));
-      return this.filtered;
-    }
-    this.filtered = this.trainings;
-    return this.filtered;
+  filterCountry(array: Training[]) {
+    let countries: Training[] = [];
+    if (this.filtersState[1].belarus) {
+        array.forEach(item => {
+        if(item.country.toLowerCase() === 'belarus'){
+          countries.push(item);
+        }})
+      }
+    if (this.filtersState[1].russia) {
+        array.forEach(item => {
+        if(item.country.toLowerCase() === 'russia'){
+          countries.push(item);
+        }})
+      }
+    if (this.filtersState[1].ukraine) {
+        array.forEach(item => {
+        if(item.country.toLowerCase() === 'ukraine'){
+          countries.push(item);
+        }})
+      }
+     return countries;
   }
 
-  filterTech(event: any){
-    if(event.source._selected){
-      const tech = event.source.value;
-      this.filtered = this.trainings.filter(item => item.technology.toLowerCase().includes(tech));
-      return this.filtered;
+  filterCity(array: Training[]) {
+    let cities: Training[] = [];
+    if (this.filtersState[2].minsk) {
+      array.forEach(item => {
+      if(item.city.toLowerCase() === 'minsk'){
+        cities.push(item);
+      }})
     }
-    this.filtered = this.trainings;
+    if (this.filtersState[2].gomel) {
+      array.forEach(item => {
+      if(item.city.toLowerCase() === 'gomel'){
+        cities.push(item);
+      }})
+    }
+    if (this.filtersState[2].brest) {
+      array.forEach(item => {
+      if(item.city.toLowerCase() === 'brest'){
+        cities.push(item);
+      }})
+    }
+    if (this.filtersState[2].kiev) {
+      array.forEach(item => {
+      if(item.city.toLowerCase() === 'kiev'){
+        cities.push(item);
+      }})
+    }
+    if (this.filtersState[2].chernigov) {
+      array.forEach(item => {
+      if(item.city.toLowerCase() === 'chernigov'){
+        cities.push(item);
+      }})
+    }
+    if (this.filtersState[2].harkov) {
+      array.forEach(item => {
+      if(item.city.toLowerCase() === 'harkov'){
+        cities.push(item);
+      }})
+    }
+    if (this.filtersState[2].moscow) {
+      array.forEach(item => {
+      if(item.city.toLowerCase() === 'moskow'){
+        cities.push(item);
+      }})
+    }
+    return cities;
+  }
+
+  filterTech(array: Training[]) {
+    let tech: Training[] = [];
+    if (this.filtersState[0].css) {
+        array.forEach(item => {
+        if(item.technology.toLowerCase() === 'css'){
+          tech.push(item);
+        }})
+      }
+    if (this.filtersState[0].html) {
+        array.forEach(item => {
+        if(item.technology.toLowerCase() === 'html'){
+          tech.push(item);
+        }})
+      }
+    if (this.filtersState[0].javascript) {
+        array.forEach(item => {
+        if(item.technology.toLowerCase() === 'javascript'){
+          tech.push(item);
+        }})
+      }
+    return tech;
+  }
+
+  checkFilters(){
+    let country = Object.values(this.filtersState[1]).some(item => item === true);
+    let city = Object.values(this.filtersState[2]).some(item => item === true);
+    let tech = Object.values(this.filtersState[0]).some(item => item === true);
+    this.activeFilters.countryFilter = country;
+    this.activeFilters.cityFilter = city;
+    this.activeFilters.techFilter = tech;
+  }
+
+  triggerFilters(){
+    this.checkFilters();
+    if(this.activeFilters.countryFilter){
+      this.filtered = this.filterCountry(this.trainings);
+    }
+    if(this.activeFilters.cityFilter){
+      this.filtered = this.filterCity(this.trainings);
+    }
+    if(this.activeFilters.techFilter){
+      this.filtered = this.filterTech(this.trainings);
+    }
+    if(Object.values(this.activeFilters).every(item => item === false)){
+      this.filtered = this.trainings;
+    }
     return this.filtered;
   }
 }
