fae27b1bbc3f92df558fc29e6cfceb84ab077847
Yaroslav Zhironkin <rainsolnce@gmail.com>
2021-04-15 15:32:09 +0300

    feat: add candidate interface for forms-service

--- src/app/core/forms.service.ts
+++ src/app/core/forms.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Observable} from 'rxjs';
+import { Candidate } from '../types/candidate';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +9,46 @@ import {Observable} from 'rxjs';
 
 export class FormsService {
   basePath = 'http://localhost:8080/api/forms';
+  candidatesList: Candidate[] = [
+    {
+      city: 'Minsk',
+      country: 'Belatus',
+      education: 'Higher',
+      email: 'itform@mail.ru',
+      englishLevel: 'A2',
+      experience: 'none',
+      filePath: 'none',
+      firstName: 'Maksim',
+      formStatus: 'APPROVED',
+      id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+      interview: {
+        admin: 'Alexey Sokolov',
+        adminFeedback: 'none',
+        id: '7fa85f64-5717-4562-b3fc-2c963f66afj9',
+        techFeedback: 'none',
+        techSpecialist: 'Dmitriy Lebedev'
+      },
+      lastName: 'Goreckiy',
+      middleName: 'Ivanovich',
+      phoneNumber: '+375297325448',
+      primarySkill: 'Python',
+      skype: 'maks4192',
+      timeForCallList: [{
+        endHour: 17,
+        formId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        id: 'kl985f64-5717-4562-b3fc-2c963f66af48',
+        startHour: 11 
+      }]
+    }
+  ];
 
   constructor(private http: HttpClient) {}
 
-  getCandidatesList(){
-    return this.http.get(this.basePath);
+  getCandidatesList(): Observable<Candidate[]>{
+    return this.http.get<Candidate[]>(this.basePath);
   }
 
-  getCandidatesListLocal(){
-
+  getCandidatesListLocal(): Candidate[] {
+    return this.candidatesList;
   }
 }
