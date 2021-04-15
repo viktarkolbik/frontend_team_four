1ecbeb7cada237ca5c443a6a1122ed7b20c702d8
RuslanTanasiichuk <74545458+RuslanTanasiichuk@users.noreply.github.com>
2021-04-15 13:16:38 +0300

    refactor: move filter into a new component

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -9,55 +9,14 @@ import { InternshipsService } from '../core/internships.service';
 })
 export class HomepageComponent implements OnInit {
   trainings: Training[];
-  cities: string[];
-  technologies: string[];
-  filters: {[key: string]: Filter} = {};
-  filteredTrainings: Training[];
+  filteredTrainings!: Training[];
   constructor(private internshipsService: InternshipsService) {
     this.trainings = this.internshipsService.getTrainingsLocal();
-    this.filteredTrainings = this.trainings;
-    let set = new Set(this.trainings.map(training => training.city));
-    this.cities = Array.from(set);
-    set = new Set(this.trainings.map(training => training.technology));
-    this.technologies = Array.from(set);
-    this.filters.locations = this.getFilter('city', this.cities);
-    this.filters.technologies = this.getFilter('technology', this.technologies);
-  }
-  getFilter(field: string, criteria: string[]): Filter{
-    const filter: Filter = {
-      field,
-      isChecked: false,
-      criteria: {}
-    };
-    for(const criterion of criteria){
-      // @ts-ignore
-      filter.criteria[criterion] = {
-        value: criterion,
-        isChecked: false,
-      };
-    }
-    return filter;
   }
   ngOnInit(): void {
     this.internshipsService.getInternshipList().subscribe((data) => console.log(data));
   }
-  updateTrainings(){
-    this.filteredTrainings = this.trainings.filter(
-      (training) => {
-        let condition = false;
-        const filters = Object.values(this.filters);
-        for (const filter of filters) {
-          const filterCriteria: Criterion[] = Object.values(filter.criteria);
-          filter.isChecked = filterCriteria.some(criterion => criterion.isChecked);
-          condition = (filter.isChecked) ?
-            filterCriteria.some(criterion => (
-              // @ts-ignore
-              criterion.isChecked && training[filter.field] === criterion.value
-            )) : true;
-          if(!condition){ break; }
-        }
-        return condition ? true : false;
-      }
-    );
+  updateTrainings(trainings: Training[]){
+    this.filteredTrainings = trainings;
   }
 }
--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -2,18 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatButtonModule } from '@angular/material/button';
 import { HomepageComponent } from './homepage.component';
 import { HomepageRoutingModule } from './homepage-routing.module';
 import { HeaderModule } from '../header/header.module';
 import { FooterModule } from '../footer/footer.module';
 import { TranslateModule } from '@ngx-translate/core';
 import { TrainingModule } from './training/training.module';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatCheckboxModule} from "@angular/material/checkbox";
-import {FormsModule} from "@angular/forms";
+import {FormsModule} from '@angular/forms';
+import {FilterModule} from './filter/filter.module';
 
 @NgModule({
   declarations: [
@@ -24,15 +20,11 @@ import {FormsModule} from "@angular/forms";
     HomepageRoutingModule,
     HeaderModule,
     FooterModule,
-    MatSelectModule,
-    MatFormFieldModule,
-    MatButtonModule,
+    FormsModule,
+    FilterModule,
     TranslateModule,
     TrainingModule,
-    HttpClientModule,
-    MatExpansionModule,
-    MatCheckboxModule,
-    FormsModule
+    HttpClientModule
   ],
   providers: [],
   exports: [
