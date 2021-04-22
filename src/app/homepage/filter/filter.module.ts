485fc213398a7642e69b51f774b5235eeda545b7
Yaroslav Zhironkin <rainsolnce@gmail.com>
2021-04-22 12:50:40 +0300

    fix: add localization for homepage

--- src/app/homepage/filter/filter.module.ts
+++ src/app/homepage/filter/filter.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FilterComponent} from './filter.component';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {TranslateModule} from '@ngx-translate/core';
-import {FormsModule} from '@angular/forms';
+import { FilterComponent } from './filter.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { TranslateModule } from '@ngx-translate/core';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
