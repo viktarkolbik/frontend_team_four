b148832fff0928495a49b4029b5c0493b672e2fe
antimage <vanua.osipovprog@gmail.com>
2021-04-22 13:27:58 +0300

    refactor: fix lint

--- src/app/trainingform/trainingform.component.ts
+++ src/app/trainingform/trainingform.component.ts
@@ -11,28 +11,6 @@ export class TrainingformComponent implements OnInit {
   form: FormGroup;
   formSelectorTime: FormGroup;
 
-  constructor() {
-    this.formSelectorTime = new FormGroup({
-    from: new FormControl(''),
-    to: new FormControl(''),
-    });
-    this.form = new FormGroup({
-      userCountry: new FormControl('', Validators.required),
-      userCity: new FormControl('', Validators.required),
-      trainingformName: new FormControl('', Validators.required),
-      trainingDescription: new FormControl(''),
-      trainingFormat: new FormControl('', Validators.required),
-      trainingLanguage: new FormControl('', Validators.required),
-      trainingRequirements: new FormControl(''),
-      trainingStart: new FormControl('', Validators.required),
-      trainingEnd: new FormControl('', Validators.required),
-      trainingRegistryStart: new FormControl('', Validators.required),
-      trainingRegistryEnd: new FormControl('', Validators.required),
-      trainingSkills: new FormControl(''),
-      publicationDate: new FormControl('current date'),
-    });
-  }
-
   countries: string[] = [
     'Беларусь',
     'Украина'
@@ -60,19 +38,52 @@ export class TrainingformComponent implements OnInit {
     'C++',
     'C#'
   ];
+
+  constructor() {
+    this.formSelectorTime = new FormGroup({
+    from: new FormControl(''),
+    to: new FormControl(''),
+    });
+    this.form = new FormGroup({
+      userCountry: new FormControl('', Validators.required),
+      userCity: new FormControl('', Validators.required),
+      trainingformName: new FormControl('', Validators.required),
+      trainingDescription: new FormControl(''),
+      trainingFormat: new FormControl('', Validators.required),
+      trainingLanguage: new FormControl('', Validators.required),
+      trainingRequirements: new FormControl(''),
+      trainingStart: new FormControl('', Validators.required),
+      trainingEnd: new FormControl('', Validators.required),
+      trainingRegistryStart: new FormControl('', Validators.required),
+      trainingRegistryEnd: new FormControl('', Validators.required),
+      trainingSkills: new FormControl(''),
+      publicationDate: new FormControl('current date'),
+    });
+  }
+
   today(): string {
-    let fullDay = new Date
-    let day = fullDay.getDate()
-    let month = fullDay.getMonth() + 1
-    let year = fullDay.getFullYear()
 
-    if ( month < 10 ) return (`${day}.0${month}.${year}`)
+    const fullDay = new Date();
 
-    if ( day < 10 ) return (`0${day}.${month}.${year}`)
+    const day = fullDay.getDate();
 
-    if ( day < 10 && month < 10 ) return (`0${day}.0${month}.${year}`)
+    const month = fullDay.getMonth() + 1;
 
-    return (`${day}.${month}.${year}`)
+    const year = fullDay.getFullYear();
+
+    if ( month < 10 ) {
+      return (`${day}.0${month}.${year}`);
+    }
+
+    if ( day < 10 ) {
+      return (`0${day}.${month}.${year}`);
+    }
+
+    if ( day < 10 && month < 10 ) {
+      return (`0${day}.0${month}.${year}`);
+    }
+
+    return (`${day}.${month}.${year}`);
   }
   ngOnInit(): void {
   }
