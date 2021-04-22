42337530d21da32130f9b9677f69ff7e8423a111
Alekskarb <korbutowski@gmail.com>
2021-04-22 17:03:15 +0300

    fix: local backend path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  backendURL: 'http://localhost:4200',
+  backendURL: 'http://localhost:8080',
   production: false
 };
 
