120f80130f472637ce347a4c4487b1bacd990a5f
Yaroslav Zhironkin <rainsolnce@gmail.com>
2021-04-22 14:39:47 +0300

    fix: get mock training

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Criterion, Filter, Training} from '../types';
+import { Training } from '../types';
 import { InternshipsService } from '../core/internships.service';
-import {ActivatedRoute} from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'ia-homepage',
@@ -15,7 +15,8 @@ export class HomepageComponent implements OnInit {
   errorMessage: string | undefined;
 
   constructor(private internshipsService: InternshipsService, private route: ActivatedRoute) {
-    this.route.data.subscribe((data) => this.trainings = data.internships);
+    // this.route.data.subscribe((data) => this.trainings = data.internships);
+    this.trainings = this.internshipsService.getTrainingsLocal();
     if (this.trainings.error) {
       this.error = this.trainings.status;
       this.errorMessage = "Что то пошло не так попробуйте обновить страницу";
