795490b71f5f22616e0d4a6476ade92b261dcb1c
Роман Неред <romaus@mail.ru>
2021-04-23 14:45:20 +0300

    refactor: fix locale homepage for error

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -12,13 +12,11 @@ export class HomepageComponent implements OnInit {
   trainings: Training[] | any;
   filteredTrainings!: Training[];
   error: number | undefined;
-  errorMessage: string | undefined;
 
   constructor(private internshipsService: InternshipsService, private route: ActivatedRoute) {
     this.route.data.subscribe((data) => this.trainings = data.internships);
     if (this.trainings.error) {
       this.error = this.trainings.status;
-      this.errorMessage = "Что то пошло не так попробуйте обновить страницу";
     }
   }
 
