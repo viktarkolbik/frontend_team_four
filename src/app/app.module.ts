8f30f409366841b81c94b15bc672ef8c3d45fbad
Alekskarb <korbutowski@gmail.com>
2021-04-30 17:38:41 +0300

    feat: lasy loading for adminpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,9 @@ import { HomepageModule } from './homepage/homepage.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateModule } from '@ngx-translate/core';
 import { I18n } from './app.i18n.config';
-import { TrainingformModule } from './trainingform/trainingform.module';
 import {HeaderModule} from './header/header.module';
 import {FooterModule} from './footer/footer.module';
 import {SpinnerModule} from './homepage/spinner/spinner.module';
-import { ErrorpageModule } from './errorpage/errorpage.module';
 import { LoginpageModule} from './loginpage/loginpage.module';
 import { AuthService } from './core/auth.service';
 import { AuthInterceptor } from './core/auth.interceptor';
@@ -30,7 +28,6 @@ import {AdminpageModule} from "./secResource/adminpage/adminpage.module";
     RegformModule,
     TrainingdescriptionModule,
     HomepageModule,
-    TrainingformModule,
     BrowserAnimationsModule,
     HttpClientModule,
     LoginpageModule,
--- src/app/secResource/adminpage/adminpage-routing.module.ts
+++ src/app/secResource/adminpage/adminpage-routing.module.ts
@@ -23,6 +23,7 @@ const routes: Routes = [
     children: [
       {
         path: 'internships',
+        loadChildren: () => import('../../trainingform/trainingform.module').then(m => m.TrainingformModule),
         component: InternshipsComponent,
         resolve: {internships: InternshipsResolver},
         canActivate: [IsAdminsGuard]
