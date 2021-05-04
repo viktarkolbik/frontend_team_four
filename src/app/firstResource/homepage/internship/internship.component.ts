9806767c9e118cb50d18d64832b443fc4323481b
Darya <70164413+Darya2311@users.noreply.github.com>
2021-05-04 18:49:35 +0300

    feat: load images for internship cards

--- src/app/firstResource/homepage/internship/internship.component.ts
+++ src/app/firstResource/homepage/internship/internship.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnChanges, OnInit} from '@angular/core';
 import {Internship} from '../../../types';
 
 @Component({
@@ -6,8 +6,27 @@ import {Internship} from '../../../types';
   templateUrl: './internship.component.html',
   styleUrls: ['./internship.component.scss']
 })
-export class InternshipComponent {
+export class InternshipComponent implements OnChanges{
   @Input() training!: Internship;
-  constructor() {
+  imgUrl = "";
+  constructor() {    
+  }
+
+  images: any = {
+    JS: "../../../../assets/icons/JS.svg",
+    JAVA: "../../../../assets/icons/java.png",
+    GO: "../../../../assets/icons/go.png",
+    QA: "../../../../assets/icons/qa.jpg",
+    DEV_OPS: "../../../../assets/icons/devops.png",
+    C_PLUS_PLUS: "../../../../assets/icons/c.png",
+    C_SHARP: "../../../../assets/icons/s-sharp.jpg"
+  }
+  ngOnChanges() {
+    this.imgUrl = this.pushImgUrl(this.training.skills[0]);
+  }
+  pushImgUrl(condition: string) {
+    if (this.training.skills.includes(condition)) {
+      return this.images[condition]
+      } 
   }
 }
--- src/app/firstResource/internshipdescription/internshipdescription.component.ts
+++ src/app/firstResource/internshipdescription/internshipdescription.component.ts
@@ -46,7 +46,6 @@ export class InternshipdescriptionComponent implements OnInit {
 
       this.techSkills = this.internship.techSkills.split(";")
       this.requirements = this.internship.requirements.split(";")
-      console.log(this.requirements);
     }
   }
 }
