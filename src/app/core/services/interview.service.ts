6a013158deac9ca149048dbce909c59b9d0a00c2
Yaroslav Zhironkin <rainsolnce@gmail.com>
2021-05-05 12:07:44 +0300

    feat: create interview-service and interview-interface

---
+++ src/app/core/services/interview.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Interview } from 'src/app/types/candidate';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class InterviewService {
+  basePath = `${environment.backendURL}/api/interviews`;
+  constructor(private http: HttpClient) {
+  }
+
+  getInterviewList(): Observable<Interview[]>{
+    return this.http.get<Interview[]>(this.basePath);
+  }
+
+  getInterviewById(id: string): Observable<Interview>{
+    return this.http.get<Interview>(`${this.basePath}/${id}`);
+  }
+
+}
