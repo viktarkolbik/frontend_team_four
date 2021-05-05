320fd5884670d44050b486acae52a0df2f0b8c69
Yaroslav Zhironkin <rainsolnce@gmail.com>
2021-05-05 20:30:26 +0300

    feat: add functionality

--- src/app/secondResource/adminpage/internlist/internlist.component.ts
+++ src/app/secondResource/adminpage/internlist/internlist.component.ts
@@ -1,14 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit,
+  Component,
+  ContentChild, EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  TemplateRef,
+  ViewChild } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatSort} from '@angular/material/sort';
 import { InterviewService } from '../../../core/services/interview.service';
 import { Interview } from '../../../types/interview';
+
 
 @Component({
   selector: 'ia-internlist',
   templateUrl: './internlist.component.html',
-  styleUrls: ['./internlist.component.scss']
+  styleUrls: ['./internlist.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
-export class InternlistComponent implements OnInit {
-  mockInterviewList = [
+
+export class InternlistComponent implements AfterViewInit, OnInit, OnChanges {
+  displayedColumns: string[] = ['admin', 'adminFeedback', 'adminInterviewDate', 'id', 'techFeedback', 'techInterviewDate', 'techSpecialist'];
+  @ViewChild(MatSort) sort!: MatSort;
+  @ContentChild(TemplateRef) template?: TemplateRef<any>;
+  @Input() Interview = [] as Interview[];
+  @Output() onSelectedCandidate: EventEmitter<Interview> = new EventEmitter<Interview>();
+  selectedCandidate: Interview | null = null;
+  @Input() selectedCandidateID?: string;
+  mockInterviewList: Interview[] = [
     {
       admin: '20981ffb-2aa8-41fe-a631-2c6f225bb1e1',
       adminFeedback: 'There is no feedback yet',
@@ -35,13 +63,27 @@ export class InternlistComponent implements OnInit {
       techInterviewDate: '2021-05-05T10:11:42.438Z',
       techSpecialist: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
     }
-  ]; 
+  ];
+  dataSource: MatTableDataSource<Interview[]>;
 
   interviewList = [] as Interview[];
-  constructor(interviewService: InterviewService) { 
+
+
+  constructor(interviewService: InterviewService) {
   }
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges) {
+    this.dataSource = new MatTableDataSource(this.mockInterviewList);
   }
-
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  ngOnInit() {
+  }
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
 }
+ 
+
--- src/app/secondResource/adminpage/internlist/internlist.module.ts
+++ src/app/secondResource/adminpage/internlist/internlist.module.ts
@@ -1,13 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InternlistComponent } from './internlist.component';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatInputModule } from '@angular/material/input';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 
 
 @NgModule({
   declarations: [InternlistComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule
+    
   ], 
   exports: [InternlistComponent]
 })
