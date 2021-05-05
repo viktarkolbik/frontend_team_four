133bd781300fc3f834f54c48a9f83091e50da25d
Yaroslav Zhironkin <rainsolnce@gmail.com>
2021-05-05 13:17:09 +0300

    feat: fill in  mock interview's data

--- src/app/secondResource/adminpage/internlist/internlist.component.ts
+++ src/app/secondResource/adminpage/internlist/internlist.component.ts
@@ -8,6 +8,34 @@ import { Interview } from 'src/app/types/candidate';
   styleUrls: ['./internlist.component.scss']
 })
 export class InternlistComponent implements OnInit {
+  mockInterviewList = [
+    {
+      admin: '20981ffb-2aa8-41fe-a631-2c6f225bb1e1',
+      adminFeedback: 'There is no feedback yet',
+      adminInterviewDate: '2021-05-05T10:11:42.438Z',
+      id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+      techFeedback: 'There is no feedback yet',
+      techInterviewDate: '2021-05-05T10:11:42.438Z',
+      techSpecialist: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+    },
+    {
+      admin: '20981ffb-2aa8-41fe-a631-2c6f225bb1e1',
+      adminFeedback: 'There is no feedback yet',
+      adminInterviewDate: '2021-05-05T10:11:42.438Z',
+      id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+      techFeedback: 'There is no feedback yet',
+      techInterviewDate: '2021-05-05T10:11:42.438Z',
+      techSpecialist: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+    },{
+      admin: '20981ffb-2aa8-41fe-a631-2c6f225bb1e1',
+      adminFeedback: 'There is no feedback yet',
+      adminInterviewDate: '2021-05-05T10:11:42.438Z',
+      id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+      techFeedback: 'There is no feedback yet',
+      techInterviewDate: '2021-05-05T10:11:42.438Z',
+      techSpecialist: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+    }
+  ]; 
 
   interviewList = [] as Interview[];
   constructor(interviewService: InterviewService) { 
