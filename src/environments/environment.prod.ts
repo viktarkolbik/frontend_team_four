08cec497441160dc3a638364ad08c33b3ca13a18
Yaroslav Zhironkin <rainsolnce@gmail.com>
2021-05-12 23:00:46 +0300

    fix: resolve conflicts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  // backendURL: 'https://intern-app-exadel-team4-be.herokuapp.com',
-  backendURL: 'http://localhost:8080',
+  backendURL: 'https://intern-app-exadel-team4-be.herokuapp.com',
   production: false
 };
