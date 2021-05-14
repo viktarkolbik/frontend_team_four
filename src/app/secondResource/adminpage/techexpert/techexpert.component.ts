a1fe39868f614a2ab3f701f3dfd37507b2ffcd0c
Yaroslav Zhironkin <rainsolnce@gmail.com>
2021-05-14 12:04:06 +0300

    feat: implement internlist for techexpert

--- src/app/secondResource/adminpage/techexpert/techexpert.component.ts
+++ src/app/secondResource/adminpage/techexpert/techexpert.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Candidate } from 'src/app/types/candidate';
 
 @Component({
   selector: 'ia-techexpert',
@@ -7,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TechexpertComponent implements OnInit {
 
-  constructor() { }
+  error?: number;
+  interns = [] as Candidate[];
+  constructor(private route: ActivatedRoute) {
+    this.route.data.subscribe(
+      (data) => {
+        if (data.interns.error) {
+          this.error = data.interns.status;
+        } else {
+          this.interns = data.interns;
+        }
+      }
+    )
+   }
 
   ngOnInit(): void {
   }
