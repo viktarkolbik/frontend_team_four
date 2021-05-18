7b1175632f6c3e44228b1f82b44362f224c2f122
Darya <70164413+Darya2311@users.noreply.github.com>
2021-05-18 13:20:30 +0300

    Refactor data (#50)
    * refactor: current data to service data
    
    * refactor: current data to service data
    
    * fix: delete useless styles
    
    * refactor: show user name istead of id in table-component
    
    * fix: admin status if data is available
    
    * fix: change user's name in the table component
    
    Co-authored-by: Yaroslav Zhironkin <rainsolnce@gmail.com>

--- src/app/core/services/internships.service.ts
+++ src/app/core/services/internships.service.ts
@@ -9,7 +9,27 @@ import { environment } from 'src/environments/environment';
 })
 export class InternshipsService {
   basePath = `${environment.backendURL}/api/internships`;
+  imgUrles: string[] = [];
+  skills: string[] = [];
   constructor(private http: HttpClient) {
+  }
+  images: { [key: string]: string } = {
+    JS: "../../../../assets/icons/js.png",
+    JAVA: "../../../../assets/icons/java.png",
+    GO: "../../../../assets/icons/go.png",
+    QA: "../../../../assets/icons/qa.jpg",
+    DEV_OPS: "../../../../assets/icons/devops.png",
+    C_PLUS_PLUS: "../../../../assets/icons/c.png",
+    C_SHARP: "../../../../assets/icons/csh.png"
+  }
+  changedSkills: { [key: string]: string } = {
+    DEV_OPS: " DevOps",
+    C_PLUS_PLUS: " C++",
+    C_SHARP: " C#",
+    JS: " JavaScript",
+    JAVA: " Java",
+    GO: " Golang",
+    QA: " QA"
   }
 
   getInternshipList(): Observable<Internship[]>{
@@ -36,6 +56,12 @@ export class InternshipsService {
       'Content-Type': 'application/json'
     });
     return this.http.put<Internship>(`${this.basePath}/${id}`, formData, {headers});
+  }
+  getImagesUrl(technology:any) {
+    return this.images[technology]
+  }
+  getChangedSkills(technology:any) {
+    return this.changedSkills[technology]
   }
 
 }
