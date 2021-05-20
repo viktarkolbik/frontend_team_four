386a006ffa93cf5b157aac5d82e8b25786580e9a
Роман Неред <romaus@mail.ru>
2021-05-20 15:30:34 +0300

    refactor: switchMap timesettingsResolver

--- src/app/core/resolvers/timesettings-resolve.service.ts
+++ src/app/core/resolvers/timesettings-resolve.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
 import {Observable, of} from 'rxjs';
-import {catchError, switchMap, tap} from 'rxjs/operators';
+import {catchError, switchMap, take, tap} from 'rxjs/operators';
 import {LoadingService} from '../services/loading.service';
 import {AuthService} from '../services/auth.service';
 import {User} from '../../types/user';
@@ -21,10 +21,10 @@ export class TimesettingsResolver implements Resolve<User> {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot ): Observable<any> {
     this.loadingService.setLoadingState(true);
-    /*
     return this.auth.getUserInfo()
-      .pipe(switchMap((data) => this.interview.getInterview(data.id, data.userRole)))
       .pipe(
+        take(1),
+        switchMap((data) => this.interview.getInterview(data.id, data.userRole)),
         tap(() => {
           this.loadingService.setLoadingState(false);
         }),
@@ -32,19 +32,7 @@ export class TimesettingsResolver implements Resolve<User> {
             this.loadingService.setLoadingState(false);
             return of (err);
           }
-        ));
-    */
-    this.auth.getUserInfo().subscribe(data => this.userInfo = data)
-    //this.interview.getInterview(this.userInfo.id, this.userInfo.userRole).subscribe(data => console.log(data))
-    return this.interview.getInterview(this.userInfo.id, this.userInfo.userRole)
-      .pipe(
-        tap(() => {
-          this.loadingService.setLoadingState(false);
-        }),
-        catchError(err => {
-            this.loadingService.setLoadingState(false);
-            return of (err);
-          }
-        ));
+        )
+      );
   }
 }
