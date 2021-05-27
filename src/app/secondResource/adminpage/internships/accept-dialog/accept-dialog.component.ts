0a1fa28f06a21be57b1f8ffbd6c85b16343d655b
RuslanTanasiichuk <RuslanTanasiichuk@users.noreply.github.com>
2021-05-27 12:58:55 +0300

    fixed loginpage, added confirmation of deletion of internship, spinner

---
+++ src/app/secondResource/adminpage/internships/accept-dialog/accept-dialog.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ia-accept-dialog',
+  templateUrl: 'accept-dialog.component.html',
+  styleUrls: ['accept-dialog.component.scss']
+})
+export class AcceptDialogComponent {}
--- src/app/secondResource/adminpage/internships/internships.module.ts
+++ src/app/secondResource/adminpage/internships/internships.module.ts
@@ -5,16 +5,27 @@ import {RouterModule} from '@angular/router';
 import {InternshipModule} from './internship/internship.module';
 import {IsAdminsGuard} from '../../../core/guards/is-admins.guard';
 import {FilterInternshipsModule} from '../../../filterInternships/filterInternships.module';
+import {AcceptDialogComponent} from "./accept-dialog/accept-dialog.component";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
-  declarations: [InternshipsComponent],
+  declarations: [
+    InternshipsComponent,
+    AcceptDialogComponent,
+  ],
   imports: [
     CommonModule,
     RouterModule,
     InternshipModule,
-    FilterInternshipsModule
+    FilterInternshipsModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatSnackBarModule
   ],
   providers: [IsAdminsGuard],
-  exports: [InternshipsComponent]
+  exports: [InternshipsComponent],
+  entryComponents: [AcceptDialogComponent]
 })
 export class InternshipsModule { }
